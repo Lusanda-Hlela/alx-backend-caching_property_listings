f65d9efae08a9177afa6a44d100117038fa78753
lusanda <hlelalusa@gmail.com>
2025-08-31 23:44:26 +0200

    Task 4: Added Redis cache metrics analysis with hit/miss ratio

--- properties/views.py
+++ properties/views.py
@@ -9,3 +9,8 @@ from .utils import get_all_properties
 def property_list(request):
     properties = get_all_properties()
     return JsonResponse({"data": list(properties)}, safe=False)
+
+
+def cache_metrics(request):
+    metrics = get_redis_cache_metrics()
+    return JsonResponse(metrics)
